94c7590b9c2707edde495cfd95a93a959d048543
craigology <ibenicraig@gmail.com>
2024-04-18 21:14:33 +0300

    String modification - Start

---
+++ work_with_variable_data/string_content_modification/Program.cs
@@ -0,0 +1,15 @@
+﻿// Exercise - Use the string's IndexOf() and Substring() helper methods
+/*
+string message = "What is the value <span>between the tags</span>?";
+
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int openingPosition = message.IndexOf(openSpan);
+int closingPosition = message.IndexOf(closeSpan);
+
+openingPosition += openSpan.Length;
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
+*/
+
