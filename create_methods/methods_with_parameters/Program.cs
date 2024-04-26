7f928b5a1563501e9f63ec018efa910195957752
craigology <ibenicraig@gmail.com>
2024-04-26 16:26:23 +0300

    Methods with parameters

---
+++ create_methods/methods_with_parameters/Program.cs
@@ -0,0 +1,106 @@
+﻿/*
+int[] schedule = { 800, 1200, 1600, 2000 };
+
+void DisplayAdjustedtimes(int[] times, int currentGMT, int newGMT)
+{
+    int diff = 0;
+    if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+    {
+        Console.WriteLine("Invalid TimeZone");
+    }
+    else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+    {
+        diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+    }
+    else
+    {
+        diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+    }
+
+    for (int i = 0; i < times.Length; i++)
+    {
+        int newTime = ((times[i] + diff)) % 24000;
+        Console.WriteLine($"{times[i]} -> {newTime}");
+    }
+}
+
+// Create an RSVP application
+
+string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+string[] rsvps = new string[10];
+int count = 0;
+
+void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+{
+    if (inviteOnly)
+    {
+        // search guestList before adding rsvp
+        bool found = false;
+        foreach (string guest in guestList)
+        {
+            if (guest.Equals(name))
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine($"Sorry, {name} is not on the guest list!");
+            return;
+        }
+    }
+
+    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+    count++;
+}
+
+void ShowRSVPs()
+{
+    Console.WriteLine("\nTotal RSVPs:");
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(rsvps[i]);
+    }
+}
+
+//Method calls
+RSVP("Rebecca", 1, "none", true);
+RSVP("Nadia", 2, "Nuts", true);
+RSVP("Linh", 2, "none", false);
+RSVP("Tony", 1, "Jackfruit", true);
+RSVP("Noor", 4, "none", false);
+RSVP("Jonte", 2, "Stone fruit", false);
+ShowRSVPs();
+*/
+// Exercise - Complete the challenge to display email addresses
+string[,] corporate =
+    {
+        {"Robert", "Bavin"}, {"Simon", "Bright"},
+        {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+        {"Sarah", "Delucchi"}, {"Sinan", "Ali"}};
+
+string[,] external =
+{
+        {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+        {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+    };
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+    DisplayEmail(first: corporate[i, 0], last: corporate[i, 1]);
+}
+
+for (int i = 0; i < external.GetLength(0); i++)
+{
+    DisplayEmail(first: external[i, 0], last: external[i, 1], domain: externalDomain);
+}
+
+void DisplayEmail(string first, string last, string domain = "contoso.com")
+{
+    string email = first.Substring(0, 2) + last;
+    email = email.ToLower();
+    Console.WriteLine($"{email}@{domain}");
+}
