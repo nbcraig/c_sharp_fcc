7839030c92912ca151792ec541a35831ca95fbd0
craigology <ibenicraig@gmail.com>
2024-05-04 20:16:29 +0300

    Implement VS Code debugger

---
+++ create_methods/guided_project/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Suppose you're an event coordinator for a petting zoo. 
+You must regularly coordinate visits from different schools and organize animal visits for several groups of students. 
+You decide to write an application that helps you plan school visits. 
+The application divides students into groups and assigns them a set of animals to visit. 
+The students will rotate groups after they visit their assigned animals. 
+You decide to randomize the order animals so that each school's visit is unique.
+
+This module will guide you through the steps required to develop your petting zoo application. 
+Your code will use methods to plan a visit for three schools, randomize the order of animals, assign students to groups, and display the results. 
+You'll use methods that accept parameters and return values, and you'll include some optional parameters as well.
+*/
+
+using System;
+
+string[] pettingZoo =
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
---
+++ debug_console_app/implement_vsc_debugger/Program.cs
@@ -0,0 +1,50 @@
+﻿/* 
+This code uses a names array and corresponding methods to display
+greeting messages
+
+
+string[] names = new string[] { "Sophia", "Andrew", "AllGreetings" };
+
+string messageText = "";
+
+foreach (string name in names)
+{
+    if (name == "Sophia")
+        messageText = SophiaMessage();
+    else if (name == "Andrew")
+        messageText = AndrewMessage();
+    else if (name == "AllGreetings")
+        messageText = SophiaMessage() + "\n\r" + AndrewMessage();
+
+    Console.WriteLine(messageText + "\n\r");
+}
+
+bool pauseCode = true;
+while (pauseCode == true) ;
+
+static string SophiaMessage()
+{
+    return "Hello, my name is Sophia.";
+}
+
+static string AndrewMessage()
+{
+    return "Hi, my name is Andrew. Good to meet you.";
+}
+*/
+/*  
+This code instantiates a value and then calls the ChangeValue method
+to update the value. The code then prints the updated value to the console.
+*/
+
+int x = 5;
+
+ChangeValue(x);
+
+Console.WriteLine(x);
+
+int ChangeValue(int value)
+{
+    value = 10;
+    return value;
+}
